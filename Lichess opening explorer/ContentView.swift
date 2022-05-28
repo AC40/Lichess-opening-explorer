5f000fd187ba0cbf6180da17ff65b96f7b1986fc
AC40 <aaron.c.richter@icloud.com>
2022-05-28 17:49:06 +0200

    Initial Commit

---
+++ Lichess opening explorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lichess opening explorer
+//
+//  Created by AC Richter on 28.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lichess opening explorer/Lichess_opening_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lichess_opening_explorerApp.swift
+//  Lichess opening explorer
+//
+//  Created by AC Richter on 28.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Lichess_opening_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
