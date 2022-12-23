f7a088b02e054068db436094366c024c4af22e72
AC40 <aaron.c.richter@icloud.com>
2022-12-23 17:48:15 +0100

    Add timer publisher

--- Lichess opening explorer/Board/Subviews/AnaylsisView.swift
+++ Lichess opening explorer/Board/Subviews/AnaylsisView.swift
@@ -12,8 +12,12 @@ struct AnaylsisView: View {
     @ObservedObject var chessboardVM: ChessboardViewModel
     
     @State private var analyze = false
-    @State private var eval: Double = 4
+    @State private var eval: Double = 0
     @State private var noCache = false
+    @State private var startTimer = false
+    @State private var stopTimer = false
+    
+    var timer = Timer.publish(every: 1, tolerance: 0.25,on: .main, in: .common).autoconnect()
     
     var body: some View {
         HStack {
@@ -26,56 +30,64 @@ struct AnaylsisView: View {
             }
             
             Spacer()
-//            Toggle("", isOn: $analyze)
-//                .onTapGesture {
-//                    fetchContinuesly()
-//                }
+            Toggle("", isOn: $analyze)
+                .onChange(of: analyze, perform: { newValue in
+                    toggleTimer(with: newValue)
+                })
+                    
+                
             
             Button("Analyze") {
                 Task {
-                    eval = await fetchAnalysis()
+                    fetchAnalysis()
                 }
             }
             .tint(.purple)
             .buttonStyle(.bordered)
             .buttonBorderShape(.roundedRectangle)
         }
-        .task {
-            eval = await fetchAnalysis()
-        }
-    }
-    
-    func fetchAnalysis() async -> Double {
-        do {
-            let cp = try await Networking.fetchCachesAnalysis(for: chessboardVM.board.asFEN()).pvs[0].cp
-            noCache = false
-            return Double(cp) / 100
-        } catch {
-            print(error)
-            noCache = true
-            return 0
-        }
-    }
-    
-    func fetchContinuesly() {
-        let timer = Timer(timeInterval: 1, repeats: analyze) { _ in
-            Task {
-                print("a")
-                do {
-                    print("b")
-                    let cp = try await Networking.fetchCachesAnalysis(for: chessboardVM.board.asFEN()).pvs[0].cp
-                    noCache = false
-                    eval = Double(cp) / 100
-                    print("Did in timer")
-                } catch {
-                    print(error)
-                    return
-                }
-            }
+        .onReceive(timer, perform: { output in
+            print("Running timer: \(output)")
+//            if stopTimer {
+//                timer.upstream.connect().cancel()
+//                stopTimer = false
+//                return
+//            }
             
+            if analyze {
+                fetchAnalysis()
+            }
+        })
+        .task {
+            fetchAnalysis()
+        }
+    }
+    
+    /// Fetches the (potentially) cached analysis of the position from the lichess server
+    func fetchAnalysis() {
+        Task {
+            do {
+                let cp = try await Networking.fetchCachesAnalysis(for: chessboardVM.board.asFEN()).pvs[0].cp
+                noCache = false
+                let res = Double(cp) / 100
+                eval = res
+            } catch {
+                print(error)
+                noCache = true
+                return
+            }
         }
         
-        timer.fire()
+    }
+    
+    /// This function starts or stops the timer, depending on what it's current state is
+    func toggleTimer(with newValue: Bool) {
+        if !newValue {
+            stopTimer = true
+        }
+//        else {
+//            timer.upstream.connect()
+//        }
     }
 }
 
